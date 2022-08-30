bac4abe9e1dd672bad65a7e16c411c2d06c6c339
Sadiya194112 <59763617+Sadiya194112@users.noreply.github.com>
2022-08-30 16:40:04 +0600

    Add files via upload

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,36 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int inf = 1e9;
+const int inf=1e7;
 int main(){
     int n,m;    cin>>n>>m;
     vector<int> dist(n+1, inf);
-    vector<vector<pair<int, int> > > g(n+1);
-    for(int i=0; i<m; i++){
+    vector<vector<pair<int,int>>> graph(n+1);
+    for(int i=0; i<m; i++) {
         int u,v,w;  cin>>u>>v>>w;
-        g[u].push_back({v,w});
-        g[v].push_back({u,w});
+        graph[u].push_back({v,w});
     }
-    int src;    cin>>src;
-    dist[src]=0;
-    set<pair<int,int> > s;
-    s.insert({0,src});
+    int source;     cin>>source;
+    dist[source]=0;
+    set<pair<int, int>> s;
+    s.insert({0, source});
     while(!s.empty()){
         auto x = *(s.begin());
         s.erase(x);
-        for(auto it:g[x.second]){
-            if(dist[it.first]>dist[it.second]+it.second){
+        for(auto it:graph[x.second]){
+            if(dist[it.first]>dist[x.second]+it.second){
                 s.erase({dist[it.first], it.first});
                 dist[it.first] = dist[x.second]+it.second;
                 s.insert({dist[it.first], it.first});
             }
         }
     }
-    for(int i=1; i<n; i++){
+    for(int i=1; i<=n; i++){
         if(dist[i]<inf){
             cout<<dist[i]<<" ";
         }
         else{
-            cout<<-1<<endl;
+            cout<<-1<<" ";
         }
     }
     return 0;
