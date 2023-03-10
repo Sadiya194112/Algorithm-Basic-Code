dfb6188789223a3eaff8752a1cf172b791d8208d
Sadiya194112 <59763617+Sadiya194112@users.noreply.github.com>
2023-03-10 11:58:21 +0600

    Set of a Set

---
+++ Set of Set.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    set<set<int>> s;
+    int n, m,i,j;
+    cin>>n>>m;
+
+    // Read in each set and its elements
+    for (i = 0; i < n; i++) {
+        vector<int> temp(m);
+        for (j = 0; j < m; j++) {
+            cin>>temp[j];
+        }
+        s.insert(set<int>(temp.begin(), temp.end()));
+    }
+
+    // Iterate over each set and its elements
+    for (auto& subset : s) {
+        cout << "Set:";
+        for (auto& ele : subset) {
+            cout<< " " <<ele;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
