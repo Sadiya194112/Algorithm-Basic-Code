95760a707432caf0d6ad2e47ca3bde5a62319c59
Sadiya Irin <59763617+Sadiya194112@users.noreply.github.com>
2023-08-30 13:05:10 +0600

    Sieve

--- Sieve.cpp
+++ Sieve.cpp
@@ -1,28 +1,44 @@
 #include<bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define ff first
+#define ss second
+#define pi acos(-1.0)
+#define yes puts("YES")
+#define no puts("NO")
+// mini = min_element(a, a+n)-a;
+// minval = *min_element(a, a+n);
+const int mod=1e9+7;
 using namespace std;
+const int N=2e5+5;
+int i,j,n;
 
-const int N = 1e7+10;
-vector<bool> isPrime(N, true);
+/*Sieve produces all primes not greater than n. Here n=500.
+  If the array contains maximum value=200000 so the value doesn't
+  contain 2 primes greater than 500. Ex: 501*501=251001 which is
+  greater than 200000.
+*/
+bool vis[501];
+vector<int>prime;
 
-
-int main(){
-    isPrime[0] = isPrime[1] = false;
-    int i,j;
-    vector<int> v;
-    for(i=2; i<N; i++){
-        if(isPrime[i]==true){
-            v.push_back(i);
-            for(j=2*i; j<N; j+=i){
-                isPrime[j]=false;
-            }
+void sieve(){
+    for(i=2; i<=500; i++){
+        if(!vis[i]){
+            prime.pb(i);
+            for(j=i*i; j<=500; j+=i) vis[j]=1;
         }
     }
-    for(i=0; i<v.size(); i++){
-            cout<<v[i]<<" ";
-    }
+    for(i=0; i<prime.size(); i++) cout<<prime[i]<<' ';
     cout<<endl;
-//    cout<<v.size()<<endl;
+}
+signed main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(NULL);
 
-    return 0;
+    sieve();
 
+/*    int tc; cin>>tc;
+    while(tc--){
+        ac();
+    }*/
 }
